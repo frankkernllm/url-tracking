fc580de4546e9353ef013ac4d59aeb59aeda5b29
frankkernllm <152027933+frankkernllm@users.noreply.github.com>
2025-06-06 23:39:49 -0700

    Create track.js

---
+++ netlify/functions/track.js
@@ -0,0 +1,28 @@
+const handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: 'Method not allowed'
+    };
+  }
+
+  try {
+    const data = JSON.parse(event.body);
+    console.log('Tracking:', data);
+    
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ success: true })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
+
+module.exports = { handler };
